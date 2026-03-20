afbc6a848951b2733f22c8e6376ff7df8a5e5b0b
mazn-developer <110087820+mazn-developer@users.noreply.github.com>
2026-03-21 01:15:07 +0400

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,7 @@ export const FOOTBALL_API_KEY = '2f79edc60ed7f63aa4af1feea0f1ff2c';
 export const FOOTBALL_API_BASE_URL = 'https://v3.football.api-sports.io';
 
 export const YT_KEYS_POOL = [
+    "AIzaSyBbhRcFh-u3bWcG8sPLNnOl5r_lMG8q7Zs",
     "AIzaSyCcB-bW1b1bSu3hzROVhSbRT-D894zHYeg", 
     "AIzaSyCtgdzDMCpCbGe1vSAu0F4wBKERC77ac6I",
     "AIzaSyAjdVZ2Rodp6ZVEF1pZT195kAtGELolxSI",
